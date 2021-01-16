a23a5c57d393027088756321a99ae91eeff04f40
klmpk23 <77089812+klmpk23@users.noreply.github.com>
2021-01-16 10:02:28 +0700

    Create Property.php

---
+++ Property.php
@@ -0,0 +1,11 @@
+<?php
+   //cara penulisan property
+   
+   class buah{         //class apel
+     var $warna;       //class merah
+     var $bentuk;     //class bulat
+     var $rasa;       //class manis
+     
+   }
+
+ ?>
